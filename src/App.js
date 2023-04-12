df3b3515f1d8ec0ece65061aa6d39ba232734e24
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-04-13 03:23:58 +0500

    btn added to test my observed issue is really a issue or just a helucination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import App2 from './App2'
+
 
 const App = () => {
   return (
     <div>
       <h1>Tao kujae</h1>
+      <button onClick={<App2 />}>Let's Go</button>
     </div>
   )
 }
---
+++ src/App2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App2 = () => {
+    return (
+        <div>
+            <h1>I'm in App2</h1>
+        </div>
+    )
+}
+
+export default App2
