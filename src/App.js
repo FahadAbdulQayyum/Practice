bc94af097dfea29737fd17d4f947979388e4be45
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-04-17 17:18:43 +0500

    lets see again

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'//v6.10.0//v5.2.0
+import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom'//v6.10.0//v5.2.0
 import App2 from './App2'
 import App3 from './App3'
 
 
 const App = () => {
   console.log('windows', window.location.hostname)
-  let host = window.location.hostname;
   return (
     <div>
       <h3>Testing Dude</h3>
       <BrowserRouter>
         <Routes>
-          {/* <Route path={`/App2`} element={<App2 />} />
-          <Route path={`/App3`} element={<App3 />} /> */}
-          <Route path={`${host}/App2`} element={<App2 />} />
-          <Route path={`${host}/App3`} element={<App3 />} />
+          <Route path={`/App2`} element={<App2 />} />
+          <Route path={`/App3`} element={<App3 />} />
+          {/* <Route path={`${host}/App2`} element={<App2 />} />
+          <Route path={`${host}/App3`} element={<App3 />} /> */}
         </Routes>
-        <div>
-          <Link to={'/App2'}>Go to App2</Link>
-        </div>
-        <Link to={'/App3'}>Go to App3</Link>
+        <Link to={'/App3'} >Go to Home</Link>
       </BrowserRouter>
     </div>
   )
--- src/App3.js
+++ src/App3.js
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom'
 
 
 const App3 = () => {
+    let host = window.location.hostname;
+
     return (
         <div>
             <h1>I'm in App3</h1>
             {/* <Link to={'App2'}>Let's Go To App2</Link> */}
+            <div>
+                {/* <Link to={`${host}/App2`}>Go to App2</Link> */}
+                <Link to={`/App2`}>Go to App2</Link>
+            </div>
         </div>
     )
 }
