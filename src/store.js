69ca1cc64a3d9dd0095f948857d099a8f7290e17
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-16 22:37:18 +0500

    removed the comments from store

--- src/store.js
+++ src/store.js
@@ -8,26 +8,3 @@ const store = configureStore({
 })
 
 export default store;
-
-// import { configureStore } from "@reduxjs/toolkit";
-// import { customReducer } from "./reducers";
-
-// const store = configureStore({
-//     reducer: {
-//         custom: customReducer,
-//     }
-// })
-
-// export default store;
-
-// // import { configureStore } from "@reduxjs/toolkit";
-// // import { customeReducer } from "./reducers";
-
-// // const store = configureStore({
-// //     reducer: {
-// //         custom: customeReducer,
-// //     },
-// // });
-
-
-// // export default store;
