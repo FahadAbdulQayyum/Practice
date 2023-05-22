2fd529a1c7a8abeb138f95b9aea28a1d51565c36
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-22 15:32:49 +0500

    previous practice progress retained

--- src/App.js
+++ src/App.js
@@ -1,70 +1,17 @@
-import React, { useState } from 'react';
-
+import React from 'react'
+import Posts from './components1/posts'
+import GlobalState from './global/globalState'
 const App = () => {
-
-  const [cntry, setCntry] = useState("India")
-
-  const obj =
-    [{
-      countries: [
-        "India",
-        "Pakistan",
-        "Bangladesh"
-      ],
-      India: [
-        "Mumbai",
-        "New Delhi"
-      ],
-      Pakistan: [
-        "Islamabad",
-        "Karachi"
-      ],
-      Bangladesh: [
-        "Dhaka",
-        "Bangali"
-      ]
-    }
-    ]
   return (
-    <div>
-      {/* First drop down starts */}
-      <h3>AsSalam</h3>
-      {obj.map((v, i) => (
-        <select key={i} onChange={e => setCntry(e?.target?.value)}>{v.countries.map((v, i) => (
-          <option key={i}>{v}</option>
-        ))}</select>
-      ))}
-      {/* First drop down ends */}
-      {/* Second drop down starts */}
-      {obj.map((v, i) => (
-        <select key={i}>{v[cntry].map((v, i) => (
-          <option key={i}>{v}</option>
-        ))}</select>
-      ))}
-      {/* Second drop down ends */}
-    </div>
+    <GlobalState>
+      <div>
+        <Posts />
+      </div>
+    </GlobalState>
   )
 }
 
-export default App;
-
-
-// *******************************
-
-// import React from 'react'
-// import Posts from './components1/posts'
-// import GlobalState from './global/globalState'
-// const App = () => {
-//   return (
-//     <GlobalState>
-//       <div>
-//         <Posts />
-//       </div>
-//     </GlobalState>
-//   )
-// }
-
-// export default App
+export default App
 
 
 
